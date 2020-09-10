df2a7a36724bc8be3c83f855695596bedadbeefd
ebresafegaga <ebresafegaga@outlook.com>
2020-09-10 22:56:14 +0100

    Fixed bug which didn't allow closures to keep arguments from functions which contained them

--- src/Eval.fs
+++ src/Eval.fs
@@ -6,6 +6,8 @@ open Ast
 // Enviroment is f.
 // Where f is a function that when given a name returns a value or not.
 //
+type Thunk = Expression 
+
 type Env = Context of (string -> Value option)
 
 and Value =
@@ -13,7 +15,7 @@ and Value =
     | StringValue of string
     | ListValue of Value list
     | UnitValue
-    | FuncValue of string list * Env * Expression
+    | FuncValue of string list * Env * Thunk
 
 // id function 
 let m () = 
@@ -44,7 +46,7 @@ let addVar (var, expr) (Context ctx) =
     Context f
 
 let addCtx (Context f) (Context g) = 
-    let  k s =
+    let k s =
         match f s with
         | None -> g s
         | Some _ as v -> v
@@ -55,26 +57,27 @@ let assoc sl vs =
     (sl, vs)
     ||> List.fold2 (fun s arg value -> addVar (arg, value) s) empty
 
-let churchable =
-    function
+let churchable = function
     | FuncValue _ -> true
     | NumericValue _ | StringValue _
     | UnitValue | FuncValue _
     | ListValue _ -> false
 
-let countParams =
-    function
+let countParams = function
     | FuncValue (ps, _, _) -> List.length ps
     | NumericValue _ | StringValue _
     | UnitValue | FuncValue _
     | ListValue _ -> 0
 
-let getFunc =
-    function
+let getFunc = function
     | FuncValue (arg, env, expr) -> arg, env, expr
     | NumericValue _ | StringValue _
     | UnitValue | FuncValue _
     | ListValue _ -> [], empty, Unit
+
+let isThunkFunc = function 
+    | Function _ | Lambda _ -> true 
+    | _ -> false 
 
 let rec eval ctx expr =
     match expr with
@@ -88,7 +91,6 @@ let rec eval ctx expr =
         | None -> 
             // Handle built-in operators
             failwithf "Unbound value %s" v
-    // There's a BUG that doesn't allow binding to get the context of functions given to it
     | Binding (a, expr) -> 
         let value, newCtx = eval ctx expr 
         UnitValue, addVar (a, value) newCtx
@@ -105,20 +107,21 @@ let rec eval ctx expr =
     | Application (f, args) -> apply ctx f args
     | Binary _ -> invalidOp "Not yet implemented"
 
-// FUNCTION Application 101:
+// FUNCTION Application 101: 
 // 1 : determine if f is a churchable
 // 2 : Check how many args f takes - params 
 // 3 : Check how many args given - args 
 // 4 : if args > params, fail 
-// 5 : if args < parms, return a curried function 
+// 5 : if args < parms, return a curried function  **
 // 6 : if equal, *apply* 
-// 7 : if f returns a lambda - g, add args to g's context
+// NOTE : if f returns a lambda - g, add args to g's context **
+//        Finally, when args = params, make sure we eval the thunk with the "deepest" context 
+//        which will hold variables of previously curried functions (if any) - This is how to APPLY. 
 
-//  BUG: apply should return the env passed to it and not the env of the function given to it.
 and apply ctx f args =
     let g = fst >> getFunc
     let func, _ = eval ctx f // do I need a ctx from this call?
-    if churchable func |> not
+    if not $ churchable func
     then failwith "This value is not a function and cannot be applied." 
     else
         let aLen = List.length args
@@ -126,25 +129,32 @@ and apply ctx f args =
         
         if aLen > pLen // more args than required?
         then
+            // printfn "here o"
             // Check if this "function value" returns a function
             // if it does, apply arguments one by one 
-            let _, context, e = getFunc func
-            let v = eval context e |> fst
-            if  v |> churchable |> not
+            let _, _, thunk = getFunc func
+            // let v = eval context e |> fst  // v |> churchable |> not // Noooooooooooo
+            if  not $ isThunkFunc thunk
             then failwith "This value is not a function and cannot be applied."
             else
                 let _, env, _ = 
                     apply ctx f [List.head args] 
                     |> fst 
                     |> getFunc
-                apply (addCtx env ctx) e (List.tail args)
+                apply (addCtx env ctx) thunk (List.tail args)
         else
+            // Curry this application.
             // TODO: Fail when args have the same name
-            let toCurry = aLen < pLen
-            let as', context, returnValue = getFunc func
-            let values = args |> List.map (eval ctx >> fst)
-            let names, rest = List.splitAt aLen as'
+            let curry = aLen < pLen
+            let arguments, context, thunk = getFunc func
+            let values = 
+                args 
+                |> List.map (eval ctx >> fst) // Eval args 
+            let names, rest = List.splitAt aLen arguments
             let context' = assoc names values
-            let r = FuncValue (rest, addCtx context context', returnValue), ctx
-            if toCurry then r
-            else eval context' returnValue
+            let enviroment = addCtx context' context
+
+            let value = FuncValue (rest, enviroment, thunk), ctx
+            
+            if curry then value
+            else eval enviroment thunk // aLen = pLen, is a simple function application, see step 6. But it must be applied with the right env.
